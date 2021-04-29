bcc1c4dfd1cccc63edd255f81bbf121b5a5eb2f3
samolego <34912839+samolego@users.noreply.github.com>
2021-04-29 15:44:30 +0200

    Architecturify

--- common/src/main/java/xyz/nucleoid/disguiselib/DisguiseLib.java
+++ common/src/main/java/xyz/nucleoid/disguiselib/DisguiseLib.java
@@ -1,13 +1,10 @@
 package xyz.nucleoid.disguiselib;
 
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.scoreboard.AbstractTeam;
 import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.scoreboard.Team;
-import xyz.nucleoid.disguiselib.command.DisguiseCommand;
 
-public class DisguiseLib implements ModInitializer {
+public class DisguiseLib {
 
 	/**
 	 * Disables collisions with disguised entities.
@@ -15,9 +12,7 @@ public class DisguiseLib implements ModInitializer {
 	 */
 	public static final Team DISGUISE_TEAM = new Team(new Scoreboard(), "");
 
-	@Override
-	public void onInitialize() {
-		CommandRegistrationCallback.EVENT.register(DisguiseCommand::register);
+	public static void init() {
 		DISGUISE_TEAM.setCollisionRule(AbstractTeam.CollisionRule.PUSH_OTHER_TEAMS);
 	}
 }
