45ea5bfcf1807bda4649793ca429803503f31fed
Patbox <39821509+Patbox@users.noreply.github.com>
2023-05-02 22:32:25 +0200

    Update to 1.19.4

--- common/src/main/java/xyz/nucleoid/disguiselib/impl/mixin/ServerPlayNetworkHandlerMixin_Disguiser.java
+++ common/src/main/java/xyz/nucleoid/disguiselib/impl/mixin/ServerPlayNetworkHandlerMixin_Disguiser.java
@@ -5,12 +5,12 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.network.Packet;
+import net.minecraft.network.listener.ClientPlayPacketListener;
+import net.minecraft.network.packet.Packet;
 import net.minecraft.network.PacketCallbacks;
 import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.network.packet.s2c.play.*;
-import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket.Entry;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.World;
@@ -26,6 +26,8 @@ import xyz.nucleoid.disguiselib.impl.mixin.accessor.*;
 import xyz.nucleoid.disguiselib.impl.packets.FakePackets;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Consumer;
 
 import static net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket.BRAND;
 import static xyz.nucleoid.disguiselib.impl.DisguiseLib.DISGUISE_TEAM;
@@ -33,6 +35,9 @@ import static xyz.nucleoid.disguiselib.impl.DisguiseLib.DISGUISE_TEAM;
 @Mixin(ServerPlayNetworkHandler.class)
 public abstract class ServerPlayNetworkHandlerMixin_Disguiser {
     @Shadow public ServerPlayerEntity player;
+
+    @Shadow public abstract void sendPacket(Packet<?> packet);
+
     @Unique
     private boolean disguiselib$skipCheck;
     @Unique
@@ -41,8 +46,6 @@ public abstract class ServerPlayNetworkHandlerMixin_Disguiser {
     private int disguiselib$qTimer;
     @Unique
     private boolean disguiselib$sentTeamPacket;
-
-    @Shadow public abstract void sendPacket(Packet<?> packet);
 
     /**
      * Checks the packet that was sent. If the entity in the packet is disguised, the
@@ -55,81 +58,108 @@ public abstract class ServerPlayNetworkHandlerMixin_Disguiser {
      * @param packet packet being sent
      */
     @Inject(
-            method = "sendPacket(Lnet/minecraft/network/Packet;Lnet/minecraft/network/PacketCallbacks;)V",
+            method = "sendPacket(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/PacketCallbacks;)V",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/network/ClientConnection;send(Lnet/minecraft/network/Packet;Lnet/minecraft/network/PacketCallbacks;)V"
+                    target = "Lnet/minecraft/network/ClientConnection;send(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/PacketCallbacks;)V"
             ),
             cancellable = true
     )
-    private void disguiseEntity(Packet<?> packet, PacketCallbacks callbacks, CallbackInfo ci) {
+    private void disguiseEntity(Packet<ClientPlayPacketListener> packet, PacketCallbacks callbacks, CallbackInfo ci) {
         if (!this.disguiselib$skipCheck) {
-            World world = this.player.getEntityWorld();
-            Entity entity = null;
-            if (packet instanceof PlayerSpawnS2CPacket) {
-                entity = world.getEntityById(((PlayerSpawnS2CPacketAccessor) packet).getId());
-            } else if (packet instanceof EntitySpawnS2CPacket) {
-                entity = world.getEntityById(((EntitySpawnS2CPacketAccessor) packet).getEntityId());
-            } else if (packet instanceof EntitiesDestroyS2CPacket && ((EntitiesDestroyS2CPacketAccessor) packet).getEntityIds().getInt(0) == this.player.getId()) {
-                ci.cancel();
-                return;
-            } else if(packet instanceof EntityTrackerUpdateS2CPacket) {
-                // an ugly fix for #6
-                int entityId = ((EntityTrackerUpdateS2CPacketAccessor) packet).getEntityId();
-                if(entityId == this.player.getId() && ((EntityDisguise) this.player).isDisguised()) {
-                    List<DataTracker.SerializedEntry<?>> trackedValues = this.player.getDataTracker().getChangedEntries();
-                    if(((EntityDisguise) this.player).getDisguiseType() != EntityType.PLAYER) {
-                        Byte flags = this.player.getDataTracker().get(EntityAccessor.getFLAGS());
+            if (packet instanceof BundleS2CPacket bundleS2CPacket) {
+                if (bundleS2CPacket.getPackets() instanceof ArrayList<Packet<ClientPlayPacketListener>> list) {
+                    var list2 = new ArrayList<Packet<ClientPlayPacketListener>>();
+                    var adder = new ArrayList<Packet<ClientPlayPacketListener>>();
+                    var atomic = new AtomicBoolean(true);
+                    for (var packet2 : list) {
+                        atomic.set(true);
+                        adder.clear();
+                        this.disguiselib$transformPacket(packet2, () -> atomic.set(false), list2::add);
 
-                        boolean removed = trackedValues.removeIf(entry -> entry.value().equals(flags));
-                        if(removed) {
-                            DataTracker.SerializedEntry<Byte> fakeInvisibleFlag = DataTracker.SerializedEntry.of(EntityAccessor.getFLAGS(), (byte) (flags | 1 << 5));
-                            trackedValues.add(fakeInvisibleFlag);
+                        if (atomic.get()) {
+                            list2.add(packet2);
                         }
+
+                        list2.addAll(adder);
                     }
-                    ((EntityTrackerUpdateS2CPacketAccessor) packet).setTrackedValues(trackedValues);
-                } else if(!((EntityDisguise) this.player).hasTrueSight()) {
-                    // Fixing "wrong data" client issue (#1)
-                    // Just prevents the client from spamming the log
-                    Entity original = world.getEntityById(entityId);
 
-                    // Only change the content if entity is disguised
-                    if(original != null && ((EntityDisguise) original).isDisguised()) {
-                        Entity disguised = ((EntityDisguise) original).getDisguiseEntity();
-                        if(disguised != null) {
-                            ((DisguiseUtils) original).updateTrackedData();
-                            List<DataTracker.SerializedEntry<?>> trackedValues = disguised.getDataTracker().getChangedEntries();
-                            ((EntityTrackerUpdateS2CPacketAccessor) packet).setTrackedValues(trackedValues);
-                        }
+                    list.clear();
+                    list.addAll(list2);
+                }
+            } else {
+                this.disguiselib$transformPacket(packet, ci::cancel, this::sendPacket);
+            }
+        }
+    }
+
+    @Unique
+    private void disguiselib$transformPacket(Packet<ClientPlayPacketListener> packet, Runnable remove, Consumer<Packet<ClientPlayPacketListener>> add) {
+        World world = this.player.getEntityWorld();
+        Entity entity = null;
+        if (packet instanceof PlayerSpawnS2CPacket) {
+            entity = world.getEntityById(((PlayerSpawnS2CPacketAccessor) packet).getId());
+        } else if (packet instanceof EntitySpawnS2CPacket) {
+            entity = world.getEntityById(((EntitySpawnS2CPacketAccessor) packet).getEntityId());
+        } else if (packet instanceof EntitiesDestroyS2CPacket && ((EntitiesDestroyS2CPacketAccessor) packet).getEntityIds().getInt(0) == this.player.getId()) {
+            remove.run();
+            return;
+        } else if(packet instanceof EntityTrackerUpdateS2CPacket) {
+            // an ugly fix for #6
+            int entityId = ((EntityTrackerUpdateS2CPacketAccessor) packet).getEntityId();
+            if(entityId == this.player.getId() && ((EntityDisguise) this.player).isDisguised()) {
+                List<DataTracker.SerializedEntry<?>> trackedValues = this.player.getDataTracker().getChangedEntries();
+                if(((EntityDisguise) this.player).getDisguiseType() != EntityType.PLAYER) {
+                    Byte flags = this.player.getDataTracker().get(EntityAccessor.getFLAGS());
+
+                    boolean removed = trackedValues.removeIf(entry -> entry.value().equals(flags));
+                    if(removed) {
+                        DataTracker.SerializedEntry<Byte> fakeInvisibleFlag = DataTracker.SerializedEntry.of(EntityAccessor.getFLAGS(), (byte) (flags | 1 << 5));
+                        trackedValues.add(fakeInvisibleFlag);
                     }
                 }
-                return;
-            } else if(packet instanceof EntityAttributesS2CPacket && !((EntityDisguise) this.player).hasTrueSight()) {
-                // Fixing #2
-                // Another client spam
-                // Entity attributes "cannot" be sent for non-living entities
-                Entity original = world.getEntityById(((EntityAttributesS2CPacketAccessor) packet).getEntityId());
-                EntityDisguise entityDisguise = (EntityDisguise) original;
+                ((EntityTrackerUpdateS2CPacketAccessor) packet).setTrackedValues(trackedValues);
+            } else if(!((EntityDisguise) this.player).hasTrueSight()) {
+                // Fixing "wrong data" client issue (#1)
+                // Just prevents the client from spamming the log
+                Entity original = world.getEntityById(entityId);
 
-                if(original != null && entityDisguise.isDisguised() && !((DisguiseUtils) original).disguiseAlive()) {
-                    ci.cancel();
-                    return;
-                }
-            } else if(packet instanceof EntityVelocityUpdateS2CPacket velocityPacket) {
-                int id = velocityPacket.getId();
-                if(id != this.player.getId()) {
-
-                    Entity entity1 = world.getEntityById(id);
-                    if(entity1 != null && ((EntityDisguise) entity1).isDisguised()) {
-                        // Cancels some client predictions
-                        ci.cancel();
+                // Only change the content if entity is disguised
+                if(original != null && ((EntityDisguise) original).isDisguised()) {
+                    Entity disguised = ((EntityDisguise) original).getDisguiseEntity();
+                    if(disguised != null) {
+                        ((DisguiseUtils) original).updateTrackedData();
+                        List<DataTracker.SerializedEntry<?>> trackedValues = disguised.getDataTracker().getChangedEntries();
+                        ((EntityTrackerUpdateS2CPacketAccessor) packet).setTrackedValues(trackedValues);
                     }
                 }
             }
+            return;
+        } else if(packet instanceof EntityAttributesS2CPacket && !((EntityDisguise) this.player).hasTrueSight()) {
+            // Fixing #2
+            // Another client spam
+            // Entity attributes "cannot" be sent for non-living entities
+            Entity original = world.getEntityById(((EntityAttributesS2CPacketAccessor) packet).getEntityId());
+            EntityDisguise entityDisguise = (EntityDisguise) original;
 
-            if(entity != null) {
-                disguiselib$sendFakePacket(entity, ci);
+            if(original != null && entityDisguise.isDisguised() && !((DisguiseUtils) original).disguiseAlive()) {
+                remove.run();
+                return;
             }
+        } else if(packet instanceof EntityVelocityUpdateS2CPacket velocityPacket) {
+            int id = velocityPacket.getId();
+            if(id != this.player.getId()) {
+
+                Entity entity1 = world.getEntityById(id);
+                if(entity1 != null && ((EntityDisguise) entity1).isDisguised()) {
+                    // Cancels some client predictions
+                    remove.run();
+                }
+            }
+        }
+
+        if(entity != null) {
+            disguiselib$sendFakePacket(entity, remove, add);
         }
     }
 
@@ -139,7 +169,7 @@ public abstract class ServerPlayNetworkHandlerMixin_Disguiser {
      * @param entity the entity that is disguised and needs to have a custom packet sent.
      */
     @Unique
-    private void disguiselib$sendFakePacket(Entity entity, CallbackInfo ci) {
+    private void disguiselib$sendFakePacket(Entity entity, Runnable remove, Consumer<Packet<ClientPlayPacketListener>> add) {
         EntityDisguise disguise = (EntityDisguise) entity;
         GameProfile profile = disguise.getGameProfile();
         Entity disguiseEntity = disguise.getDisguiseEntity();
@@ -153,7 +183,7 @@ public abstract class ServerPlayNetworkHandlerMixin_Disguiser {
         this.disguiselib$skipCheck = true;
         if (disguise.getDisguiseType() == EntityType.PLAYER) {
             PlayerListS2CPacket packet = new PlayerListS2CPacket(PlayerListS2CPacket.Action.ADD_PLAYER, (ServerPlayerEntity) disguiseEntity);
-            this.sendPacket(packet);
+            add.accept(packet);
 
             if (!(entity instanceof PlayerEntity)) {
                 var playerRemovePacket = new PlayerRemoveS2CPacket(new ArrayList<>(Collections.singletonList(profile.getId())));
@@ -172,20 +202,20 @@ public abstract class ServerPlayNetworkHandlerMixin_Disguiser {
                         ((EntitySpawnS2CPacketAccessor) spawnPacket).setUuid(disguiseEntity.getUuid());
                     }
                     disguiseEntity.startRiding(this.player, true);
-                    this.sendPacket(spawnPacket);
+                    add.accept((Packet<ClientPlayPacketListener>) spawnPacket);
 
                     TeamS2CPacket joinTeamPacket = TeamS2CPacket.changePlayerTeam(DISGUISE_TEAM, this.player.getGameProfile().getName(), TeamS2CPacket.Operation.ADD); // join team
-                    this.sendPacket(joinTeamPacket);
+                    add.accept(joinTeamPacket);
                 }
             }
-            ci.cancel();
+            remove.run();
         } else if(disguise.isDisguised()) {
             //this.player.getX()
             //ArmorStandEntity fakeStand = new ArmorStandEntity(this.player.world, );
             //fakeStand.startRiding(fakeStand, true);
             //new EntitySpawnS2CPacket(fakeStand);
-            this.sendPacket(spawnPacket);
-            ci.cancel();
+            add.accept((Packet<ClientPlayPacketListener>) spawnPacket);
+            remove.run();
         }
 
 
@@ -197,7 +227,7 @@ public abstract class ServerPlayNetworkHandlerMixin_Disguiser {
         method = "onPlayerMove(Lnet/minecraft/network/packet/c2s/play/PlayerMoveC2SPacket;)V",
         at = @At(
                 value = "INVOKE",
-                target = "Lnet/minecraft/network/NetworkThreadUtils;forceMainThread(Lnet/minecraft/network/Packet;Lnet/minecraft/network/listener/PacketListener;Lnet/minecraft/server/world/ServerWorld;)V",
+                target = "Lnet/minecraft/network/NetworkThreadUtils;forceMainThread(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/listener/PacketListener;Lnet/minecraft/server/world/ServerWorld;)V",
                 shift = At.Shift.AFTER
         )
     )
